084bcf31ed00b0c5cbf3c4f32693b91f2ab7a204
karl <karl@51ab786b-562a-0410-8f09-fdec1c1f4778>
2004-10-24 20:20:39 +0000

    simplified code

--- src/ssh/v2/DHKeyExchange.java
+++ src/ssh/v2/DHKeyExchange.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of "MidpSSH".
  * 
- * This file was adapted from Java Secure Channel (www.jcraft.com/jsch/) for
+ * This file was greatly adapted from Java Secure Channel (www.jcraft.com/jsch/) for
  * MidpSSH by Karl von Randow
  * 
  * Copyright (c) 2002,2003,2004 ymnk, JCraft,Inc. All rights reserved.
@@ -126,9 +126,6 @@ public class DHKeyExchange {
 	 * @throws Exception
 	 */
 	public boolean next(byte[] K_S, byte[] f, byte[] sig_of_h) {
-
-		int i, j;
-
 		// K_S is server_key_blob, which includes ....
 		// string ssh-dss
 		// impint p of dsa
@@ -171,65 +168,17 @@ public class DHKeyExchange {
 		H = new byte[sha.getDigestSize()];
 		sha.doFinal(H, 0);
 
-		//System.out.print("H -> "); //dump(H, 0, H.length);
-
-		/*
-		 * SshPacket2 pp = new SshPacket2( null ); pp.putBytes( K_S );
-		 * System.out.println( "READ \"" + pp.getString() + "\"" );
-		 * pp.getMpInt(); System.out.println( "READ p" ); pp.getMpInt();
-		 * System.out.println( "READ q" ); pp.getMpInt(); System.out.println(
-		 * "READ g" ); pp.getMpInt(); System.out.println( "READ y" );
-		 */
-
-		i = 0;
-		j = 0;
-		j = ((K_S[i++] << 24) & 0xff000000) | ((K_S[i++] << 16) & 0x00ff0000)
-				| ((K_S[i++] << 8) & 0x0000ff00) | ((K_S[i++]) & 0x000000ff);
-		String alg = new String(K_S, i, j);
-		i += j;
-
-		boolean result = false;
-
-		if (alg.equals("ssh-dss")) {
-			byte[] q = null;
-			byte[] tmp;
-			byte[] p;
-			byte[] g;
-
-			j = ((K_S[i++] << 24) & 0xff000000)
-					| ((K_S[i++] << 16) & 0x00ff0000)
-					| ((K_S[i++] << 8) & 0x0000ff00)
-					| ((K_S[i++]) & 0x000000ff);
-			tmp = new byte[j];
-			System.arraycopy(K_S, i, tmp, 0, j);
-			i += j;
-			p = tmp;
-			j = ((K_S[i++] << 24) & 0xff000000)
-					| ((K_S[i++] << 16) & 0x00ff0000)
-					| ((K_S[i++] << 8) & 0x0000ff00)
-					| ((K_S[i++]) & 0x000000ff);
-			tmp = new byte[j];
-			System.arraycopy(K_S, i, tmp, 0, j);
-			i += j;
-			q = tmp;
-			j = ((K_S[i++] << 24) & 0xff000000)
-					| ((K_S[i++] << 16) & 0x00ff0000)
-					| ((K_S[i++] << 8) & 0x0000ff00)
-					| ((K_S[i++]) & 0x000000ff);
-			tmp = new byte[j];
-			System.arraycopy(K_S, i, tmp, 0, j);
-			i += j;
-			g = tmp;
-			j = ((K_S[i++] << 24) & 0xff000000)
-					| ((K_S[i++] << 16) & 0x00ff0000)
-					| ((K_S[i++] << 8) & 0x0000ff00)
-					| ((K_S[i++]) & 0x000000ff);
-			tmp = new byte[j];
-			System.arraycopy(K_S, i, tmp, 0, j);
-			i += j;
-			byte[] y = tmp;
-			//	SignatureDSA sig=new SignatureDSA();
-			//	sig.init();
+		SshPacket2 pp = new SshPacket2( null );
+		pp.putBytes( K_S );
+		String alg = pp.getString();
+		boolean result;
+		
+		if ( alg.equals( "ssh-dss" ) ) {
+			byte [] p = pp.getByteString();
+			byte [] q = pp.getByteString();
+			byte [] g = pp.getByteString();
+			byte [] y = pp.getByteString();
+			
 			DSASigner sig = new DSASigner();
 			sig.setY(new BigInteger(y));
 			sig.setP(new BigInteger(p));
@@ -237,9 +186,12 @@ public class DHKeyExchange {
 			sig.setG(new BigInteger(g));
 
 			result = sig.verifySignature(H, sig_of_h);
-		} else {
-			System.out.println("unknow alg");
 		}
+		else {
+			System.out.println("unknow alg");
+			result = false;
+		}
+		
 		return result;
 	}
 
